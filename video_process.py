bb824e84a44f34b85b232d7a1484c0f6411d27ba
yvette <yvette.lrox1@gmail.com>
2019-03-02 10:57:45 -0800

    okay NOW video processing works

--- video_process.py
+++ video_process.py
@@ -8,18 +8,16 @@ def vid_to_frames(filename, num_frames):
     vidcap = cv2.VideoCapture(filename)
     framerate = 24
     
-    frame_id = vidcap.get(1)
     success, image = vidcap.read()
 
     count = 0
     while success and count <= num_frames * 24:
-        if frame_id % 24 == 0:
+        if count % 24 == 0:
             frame_name = path + '/frames/frame%d.jpg' % count
             print(frame_name)
             cv2.imwrite(frame_name, image)
             print('Wrote ', frame_name)
         
-        frame_id = vidcap.get(1)  # current frame number
         success, image = vidcap.read()
         
         print('Read a new frame: ', success)
@@ -35,9 +33,5 @@ def frame_to_blackboard(frame):
     pass
 
 
-def main(filename):
-    vid_to_frames(filename, 50)
-
-
 if __name__ == '__main__':
-    main('tests/vid/IMG_2645.MOV')
+    vid_to_frames('tests/vid/IMG_2645.MOV', 100)
