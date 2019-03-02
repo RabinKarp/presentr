fae653c8eaf58e830141e38524460b36822ff378
yvette <yvette.lrox1@gmail.com>
2019-03-02 10:10:49 -0800

    hopefully pulls an image a second but i didn't test this

--- video_process.py
+++ video_process.py
@@ -1,17 +1,31 @@
 import cv2
 import os
+import math
 
 def vid_to_frames(filename):
     path = os.path.dirname(filename)
 
     vidcap = cv2.VideoCapture(filename)
+    framerate = vidcap.get(24)
+    
+    frame_id = vidcap.get(1)
     success, image = vidcap.read()
+    
     count = 0
     while success:
-        cv2.imwrite('path/frames/frame%d.jpg'% count, image)
-        success image = vidcap.read()
+        if frame_id % math.floor(framerate) == 0:
+            cv2.imwrite('path/frames/frame%d.jpg'% count, image)
+            print('Wrote frame', count)
+        
+        frame_id = vidcap.get(1)  # current frame number
+        success, image = vidcap.read()
+        
         print('Read a new frame: ', success)
         count += 1
+
+    vidcap.release()
+    cv2.destroyAllWindows()
+    print('Done!')
 
 
 def frame_to_blackboard(frame):
@@ -24,4 +38,4 @@ def main(filename):
 
 
 if __name__ == '__main__':
-    main()
+    pass
